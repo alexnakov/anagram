9bf8d6b93fe6c224e1497e4188266234257ebbbf
alexnakov <alex.nakov3@gmail.com>
2023-12-07 20:38:35 +0000

    found a logic for detecting keys

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,60 +1,54 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import BoardSVG from '../components/BoardSVG.js'
 import { motion, spring } from 'framer-motion'
 
 
 export default function Game() {
-  const [box1Height, setBox1Height] = useState(0)
-  const [box1Width, setBox1Width] = useState(0)
-  const [box2Height, setBox2Height] = useState(0)
-  const [box2Width, setBox2Width] = useState(100)
-  const [box3Height, setBox3Height] = useState(100)
-  const [box3Width, setBox3Width] = useState(0)
-  const [box4Height, setBox4Height] = useState(100)
-  const [box4Width, setBox4Width] = useState(100)
+  // Will we index things with 0 to start with
+  const [nextFreeTopBoxPos, setNextFreeTopBoxPos] = useState([0, 0])
+  const [nextFreeBottomBoxPos, setNextFreeBottomBoxPos] = useState([0, 0])
 
-  const [clicked, setClicked] = useState(false)
 
-  // 1 Style object for all 4 boxes
-  // const boxStyle = {
-  //   width: '50px', height: '50px', 
-  //   backgroundColor: 'green', 
-  //   position: 'absolute',
-  // }
+  const [box1Pos, setBox1Pos] = useState([0, 100])
+  const [box2Pos, setBox2Pos] = useState([100, 100])
+  const [box3Pos, setBox3Pos] = useState([200, 100])
+  const [box4Pos, setBox4Pos] = useState([300, 100])
 
-  const box1Pos = {
+  const commonStyles = {
     width: '50px', height: '50px', 
     backgroundColor: 'red', 
     position: 'absolute',
-    top: box1Height + "px",
-    left: box1Width + "px",
+    textAlign: 'center',
+  }
+  const box1Style = {
+    ...commonStyles,
+    top: box1Pos[1] + "px",
+    left: box1Pos[0] + "px",
+  }
+  const box2Style = {
+    ...commonStyles,
+    top: box2Pos[1] + "px",
+    left: box2Pos[0] + "px",
+  }
+  const box3Style = {
+    ...commonStyles,
+    top: box3Pos[1] + "px",
+    left: box3Pos[0] + "px",
+  }
+  const box4Style = {
+    ...commonStyles,
+    top: box4Pos[1] + "px",
+    left: box4Pos[0] + "px",
   }
 
-  const box2Pos = {
-    width: '50px', height: '50px', 
-    backgroundColor: 'green', 
-    position: 'absolute',
-    top: box2Height + "px",
-    left: box2Width + "px",
-  }
+  useEffect(() => {
+    document.addEventListener('keydown', detectKeyDown, true)
+  }, [])
 
-  const box3Pos = {
-    width: '50px', height: '50px', 
-    backgroundColor: 'brown', 
-    position: 'absolute',
-    top: box3Height + "px",
-    left: box3Width + "px",
+  const detectKeyDown = e => {
+    console.log(e.key)
   }
-
-  const box4Pos = {
-    width: '50px', height: '50px', 
-    backgroundColor: 'orange', 
-    position: 'absolute',
-    top: box4Height + "px",
-    left: box4Width + "px",
-  }
-
 
   const spring = {
     type: "spring",
@@ -62,40 +56,20 @@ export default function Game() {
     damping: 100,
   };
 
-  function moveBoxes(e) {
+  const excite = (e) => {
     e.preventDefault()
-    if (!clicked) {
-      setBox1Height(100)
-      setBox2Height(100)
-      setBox3Height(0)
-      setBox4Height(0)
-    
-      setBox1Width(100)
-      setBox3Width(100)
-      setBox2Width(0)
-      setBox4Width(0)
-    } else {
-      setBox1Height(0)
-      setBox2Height(0)
-      setBox3Height(100)
-      setBox4Height(100)
-    
-      setBox1Width(0)
-      setBox3Width(0)
-      setBox2Width(100)
-      setBox4Width(100)
-    }
-    setClicked(!clicked)
+    console.log('p')
+    console.log(e.which || e.keyCode || 0)
   }
   
   return (
-    <div onClick={e => moveBoxes(e)} style={{width: '100vw', height: '100vh', display: 'grid', placeItems: 'center'}}>
-      <div style={{border: '1px solid red', width: '150px', height: '150px', position: 'relative'}}>
+    <div style={{width: '100vw', height: '100vh', display: 'grid', placeItems: 'center'}}>
+      <div style={{border: '1px solid red', width: '350px', height: '150px', position: 'relative'}}>
         <BoardSVG />
-        {/* <motion.div className='box' layout transition={spring} style={box1Pos}></motion.div>
-        <motion.div className='box' layout transition={spring} style={box2Pos}></motion.div>
-        <motion.div className='box' layout transition={spring} style={box3Pos}></motion.div>
-        <motion.div className='box' layout transition={spring} style={box4Pos}></motion.div> */}
+        <motion.div className='box' layout transition={spring} style={box1Style}>A</motion.div>
+        <motion.div className='box' layout transition={spring} style={box2Style}>N</motion.div>
+        <motion.div className='box' layout transition={spring} style={box3Style}>E</motion.div>
+        <motion.div className='box' layout transition={spring} style={box4Style}>M</motion.div>
       </div>
     </div>
   )
