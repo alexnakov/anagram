5d23f278ca801777ce7b9f6840385b91d31252ec
alexnakov <alex.nakov3@gmail.com>
2023-12-10 13:42:45 +0000

    Updating an arr of latters

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -6,22 +6,58 @@ import useKeyPress from 'react-use-keypress'
 
 
 export default function Game() {
-  
+  const [layoutArr, setLayoutArr] = useState([['', '', '', ''], ['a', 'n', 'e', 'm']])
 
-  const ALLKEYS = 'qwertyuiopasdfghjklzxcvbnm'.split('')
-
-  useKeyPress(ALLKEYS, e => {
-    console.log(e.key)
+  // Updating state of key pressed
+  const ALLKEYS = 'name'.split('')
+  useKeyPress([...ALLKEYS, 'Backspace'], e => {
+    moveLetters(e.key)
   })
+
+  const moveLetters = currentK => { 
+    if (currentK === 'Backspace') {
+      let newLayout = [...layoutArr]
+      for (let i = newLayout[0].length - 1; i > -1; i--) {
+        if (newLayout[0][i] !== '') {
+          const firstEmptyIndex = newLayout[1].indexOf('')
+          newLayout[1][firstEmptyIndex] = newLayout[0][i]
+          newLayout[0][i] = ''
+          setLayoutArr(newLayout)
+          console.log(layoutArr)
+          break
+        }
+      }
+    }
+    else if (layoutArr[0].includes(currentK)) {
+      let newLayout = [...layoutArr]
+      const selectedLetterIndex = newLayout[0].indexOf(currentK)
+      const firstEmptySpaceIndex = newLayout[1].indexOf('')
+  
+      newLayout[0][selectedLetterIndex] = ''
+      newLayout[1][firstEmptySpaceIndex] = currentK 
+      setLayoutArr(newLayout)
+      console.log(layoutArr)
+    } 
+    else if (layoutArr[1].includes(currentK)) {
+      let newLayout = [...layoutArr]
+      const selectedLetterIndex = newLayout[1].indexOf(currentK)
+      const firstEmptySpaceIndex = newLayout[0].indexOf('')
+  
+      newLayout[1][selectedLetterIndex] = ''
+      newLayout[0][firstEmptySpaceIndex] = currentK 
+      setLayoutArr(newLayout)
+      console.log(layoutArr)
+    }
+  }
   
   return (
     <div style={{width: '100vw', height: '100vh', display: 'grid', placeItems: 'center'}}>
       <div style={{border: '1px solid red', width: '350px', height: '150px', position: 'relative'}}>
         <BoardSVG />
-        <Letter letterText="a" x="0" y="100" n={n} setN={setN} currentK={currentK}/>
-        <Letter letterText="n" x="100" y="100" n={n} setN={setN} currentK={currentK}/>
-        <Letter letterText="e" x="200" y="100" n={n} setN={setN} currentK={currentK}/>
-        <Letter letterText="m" x="300" y="100" n={n} setN={setN} currentK={currentK}/>
+        <Letter letterText="a" x="0" y="100" />
+        <Letter letterText="n" x="100" y="100" />
+        <Letter letterText="e" x="200" y="100" />
+        <Letter letterText="m" x="300" y="100" />
       </div>
     </div>
   )
