16f851e087f76b4652a68fb9130781e5c7b33fca
alexnakov <alex.nakov3@gmail.com>
2023-12-24 12:47:01 +0000

    Feature: Added a transparent autofocus input to track keypress

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -45,7 +45,8 @@ export default function Game() {
   const [charStates, setCharStates] = useState(arrayOfMapsFromChars)
 
   return (
-    <div  style={{width: '100vw', height: '100vh', display: 'grid', placeItems: 'center'}}>
+    <div style={{width: '100vw', height: '100vh', display: 'grid', placeItems: 'center'}}>
+      <input autoFocus onKeyUp={e => console.log(2)} style={{border: '1px solid red', height: '100px', position: 'absolute', outline: 'none', caretColor: 'transparent', color: '#000', opacity: 0.5, top: '100px', display: 'block'}} />
       <div style={{border: '1px solid red', width: '610px', height: '150px', position: 'relative'}}>
         <BoardSVG />
 
